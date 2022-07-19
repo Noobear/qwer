494e1ca5eca104cc7b39d6169673057d8ec438e8
tmshdnaos45 <tmshdnaos45@naver.com>
2022-07-19 19:48:37 +0900

    7/19 evening

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import React from 'react';
+import logo from './photo/wheel1.svg';
+import HomeLoginBar from './H_LoginBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h3>Stop! Bus!</h3>
+          <HomeLoginBar></HomeLoginBar>
+        </header>
+      </div>
   );
 }
 
